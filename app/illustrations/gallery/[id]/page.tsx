f618856e49ab9c87ac04e8108acbbece7b2f086f
dkm94 <diane.mpacko@gmail.com>
2023-08-16 22:50:19 +0200

    + handle show/hide gallery's image description box / OK

--- app/illustrations/gallery/[id]/page.tsx
+++ app/illustrations/gallery/[id]/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import React, { useState } from 'react';
+import Image from 'next/image';
+import { cardo } from '@/app/fonts';
+
 import { PageContent, PageHeader } from '@/components';
 import { galleries } from '@/constants';
-import { cardo } from '@/app/fonts';
-import Image from 'next/image';
 
 interface GalleryPageProps {
     params: {
@@ -14,7 +15,21 @@ interface GalleryPageProps {
 
 const GalleryPage = ({ params }: GalleryPageProps) => {
 
-	const [ visible, setVisible ] = useState(false);
+	const [ selectedId, setSelectedId ] = useState<string | number>('');
+
+	const getSelectedId = (id: number | string) => {
+		setSelectedId(id);
+	};
+
+	const showMore = (id: number | string) => (e: React.MouseEvent<HTMLElement>) => {
+		e.preventDefault();
+		getSelectedId(id);
+	};
+
+	const showLess = (): void => {
+		getSelectedId('');
+	};
+
 	const filteredGallery = galleries.filter(gallery => gallery.id === params.id);
 
 	return (
@@ -28,21 +43,21 @@ const GalleryPage = ({ params }: GalleryPageProps) => {
 				<div className='relative'>
 					{filteredGallery?.map((gallery, index) => (
 						<div key={index} className='grid grid-cols-1 gap-2'>
-							{gallery?.images?.map(({ title, description, url }, index) => 
+							{gallery?.images?.map(({ title, description, url, id }, index) => 
 								<div key={index} className='relative'>
 									<Image src={url} alt={title} className='w-full h-full object-cover' width={200} height={300} />
-									{visible ? <div className='bg-gradient-to-t from-gray-400 to-transparent absolute bottom-0 flex flex-col w-full h-52 gap-2 bg-opacity-80 p-4 justify-between self-end'>
+									{selectedId == id ? <div className='bg-gradient-to-t from-gray-400 to-transparent absolute bottom-0 flex flex-col w-full h-52 gap-2 bg-opacity-80 p-4 justify-between self-end'>
 										<div className='flex flex-col gap-2'>
 											<div className='font-bold'>{title}</div>
 											<div>{description}</div>
 										</div>
 										<div className='w-full flex justify-end'>
-											<button type='button' className='text-white bg-slate-900 uppercase w-fit py-1 px-4' onClick={() => setVisible(false)}>
+											<button type='button' className='text-white bg-slate-900 uppercase w-fit py-1 px-4' onClick={showLess}>
 											See less
 											</button>
 										</div>
 									</div> : <div className='absolute bottom-0 w-full justify-end p-4 h-fit flex' >
-										<button type='button' className='text-white bg-slate-900 uppercase w-fit py-1 px-4' onClick={() => setVisible(true)} >See more</button></div>}
+										<button type='button' className='text-white bg-slate-900 uppercase w-fit py-1 px-4' onClick={showMore(id)} >See more</button></div>}
 								</div>
 							)}
 						</div>
