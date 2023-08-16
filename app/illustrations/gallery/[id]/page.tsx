9a6ba6f30a8c5a8aaed26d22802c8c66d0dda9b0
dkm94 <diane.mpacko@gmail.com>
2023-08-16 21:39:45 +0200

    + handle image gallery description box visibility with buttons

--- app/illustrations/gallery/[id]/page.tsx
+++ app/illustrations/gallery/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { PageContent, PageHeader } from '@/components';
 import { galleries } from '@/constants';
 import { cardo } from '@/app/fonts';
@@ -14,6 +14,7 @@ interface GalleryPageProps {
 
 const GalleryPage = ({ params }: GalleryPageProps) => {
 
+	const [ visible, setVisible ] = useState(false);
 	const filteredGallery = galleries.filter(gallery => gallery.id === params.id);
 
 	return (
@@ -30,17 +31,18 @@ const GalleryPage = ({ params }: GalleryPageProps) => {
 							{gallery?.images?.map(({ title, description, url }, index) => 
 								<div key={index} className='relative'>
 									<Image src={url} alt={title} className='w-full h-full object-cover' width={200} height={300} />
-									<div className='description-box absolute bottom-0 flex flex-col w-full h-52 gap-2 bg-opacity-80 p-4 justify-between self-end'>
+									{visible ? <div className='bg-gradient-to-t from-gray-400 to-transparent absolute bottom-0 flex flex-col w-full h-52 gap-2 bg-opacity-80 p-4 justify-between self-end'>
 										<div className='flex flex-col gap-2'>
 											<div className='font-bold'>{title}</div>
 											<div>{description}</div>
 										</div>
 										<div className='w-full flex justify-end'>
-											<button className='text-white bg-slate-900 uppercase w-fit py-1 px-4'>
+											<button type='button' className='text-white bg-slate-900 uppercase w-fit py-1 px-4' onClick={() => setVisible(false)}>
 											See less
 											</button>
 										</div>
-									</div>
+									</div> : <div className='absolute bottom-0 w-full justify-end p-4 h-fit flex' >
+										<button type='button' className='text-white bg-slate-900 uppercase w-fit py-1 px-4' onClick={() => setVisible(true)} >See more</button></div>}
 								</div>
 							)}
 						</div>
