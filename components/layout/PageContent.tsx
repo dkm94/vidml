6be30dee27c33c1305217c3795824fd1ec7d0c73
dkm94 <diane.mpacko@gmail.com>
2023-08-16 03:20:11 +0200

    + fixed PageContent background image

--- components/layout/PageContent.tsx
+++ components/layout/PageContent.tsx
@@ -3,8 +3,9 @@ import { PageContentProps } from '@/types';
 
 const PageContent = ({ children, backgroundImage }: PageContentProps) => {
 	return (
-		<section className={`page-content min-h-[600px] relative bg-${ backgroundImage }`}>
-			{children}
+		<section className='page-content min-h-[600px] relative'>
+			<div className={`bg-${ backgroundImage } absolute top-0 bottom-0 left-0 right-0 content-[""] opacity-40`} aria-hidden="true" />
+			<div className='relative'>{children}</div>
 		</section>
 	);
 };
