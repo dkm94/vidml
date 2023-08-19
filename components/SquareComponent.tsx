d47a6caedb2f78cdbd3196a6ec5861cb34dfedec
dkm94 <diane.mpacko@gmail.com>
2023-08-20 00:10:06 +0200

    + added "About me" page content + added portrait

--- components/SquareComponent.tsx
+++ components/SquareComponent.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const SquareComponent = ({ isLink, path, containerStyle, childStyle, text }: SquareComponentsProps) => {
 	return (
 		<div className={`absolute border-solid border-8 left-2/4 top-2/4 translate-x-[-50%] translate-y-[-50%] px-8 py-3 ${ containerStyle }`}>
-			{isLink ? <Link href={{ pathname: path }} className={`text-4xl square-title ${ childStyle }`}>{text}</Link> : <h1 className={`text-4xl square-title ${ childStyle }`}>{text}</h1>}
+			{isLink ? <Link href={{ pathname: path }} className={`text-4xl square-title ${ childStyle }`}>{text}</Link> : <h1 className={`text-3xl square-title ${ childStyle }`}>{text}</h1>}
 		</div>
 	);
 };
