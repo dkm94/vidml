5efebfe064902191db6fe6fce5889a274c21bbcc
dkm94 <diane.mpacko@gmail.com>
2024-02-16 03:36:01 +0100

    + disable ESlint for unused values (for production purpose)

--- components/buttons/SubmitButton.tsx
+++ components/buttons/SubmitButton.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { ComponentPropsWithoutRef } from 'react';
 
 interface SubmitButtonProps extends ComponentPropsWithoutRef<'button'>{};
