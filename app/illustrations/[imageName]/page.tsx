ffe4fe83788821e34b864ea17c6df879f516d179
dkm94 <diane.mpacko@gmail.com>
2024-02-22 16:22:45 +0100

    url issue

--- app/illustrations/[imageName]/page.tsx
+++ app/illustrations/[imageName]/page.tsx
@@ -1,3 +1,23 @@
-export default function IllustrationPage () {
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+interface IllustrationPageProps {
+	params: {
+		imageName: string;
+	};
+}
+
+export default function IllustrationPage (props: IllustrationPageProps) {
+	console.log('🚀 ~ :', props);
+	const { imageName } = props.params;
+	const router = useRouter();
+
+	useEffect(() => {
+		router.replace(`/illustrations/${ imageName }`);
+		
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 	return;
 };
