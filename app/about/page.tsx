1de935f76e11de9920ff7a4989ee55ec2ac582ac
dkm94 <diane.mpacko@gmail.com>
2024-04-26 23:01:15 +0200

    + changed about me intro content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -23,9 +23,9 @@ const AboutPage = () => {
 						<div className='relative flex items-center'>
 							<div className='absolute bg-white opacity-50 w-full h-full' aria-hidden="true"/>
 							<div className='relative px-10 py-16 flex flex-col gap-6'>
-								<p className='text-pretty'>{'Artiste autodidacte originaire de la région parisienne, de par mes origines modestes, je n\'ai jamais fréquenté d\'école d\'art ni suivi de cours formels. Ma passion pour l\'illustration et la mode a été le moteur de mon apprentissage, façonné par une persévérance inébranlable et des expériences enrichissantes avec des entreprises qui ont eu confiance en mon talent.'}</p>
-								<p>{'Mon parcours témoigne de mon dévouement envers mon art, résultant non seulement de ma créativité innée, mais également des opportunités que des collaborations professionnelles ont apportées. Je vous remercie sincèrement de visiter ma galerie et de partager cette aventure avec moi.'}</p>
-								<p>{'Prenez soin de vous,'}</p>
+								<p className='text-pretty'>{'C\'est avec un grand plaisir que je vous accueille sur ma galerie.'}</p>
+								<p>{'Je m\'appelle Virginie, j\'ai grandi dans la région parisienne. Originaire d\'une famille modeste, je n\'ai jamais été en école d\'art ni pris de cours. Je suis une autodidacte, passionnée d\'illustrations et de mode depuis toujours. J\'ai appris à force de persévérance, mais aussi d\'expériences grâce à des entreprises qui m\'ont fait confiance.'}</p>
+								<p>{'Merci pour votre visite. Prenez soin de vous,'}</p>
 								<p className='text-end'>{'Virgine DML'}</p>
 							</div>
 						</div>
