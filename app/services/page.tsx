6aa4fb6391080bf0bedcaac39cd5d17c0553c040
dkm94 <diane.mpacko@gmail.com>
2024-04-28 20:24:06 +0200

    + updated /services

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -17,18 +17,18 @@ function ServicesPage(){
 					<h2 className='text-balance'>Des services sur mesure pour vous aider à réaliser le projet de vos rêves</h2>
 				</div> */}
 				<section className='flex flex-col gap-10 max-w-4xl w-full mt-18 text-slate-100 sm:border border-x-slate-50 border-solid'>
-					<div className={`services-header text-center pt-8 pb-8 bg-slate-50 text-black text-2xl ${ bebasNeue.className }`}>Tarifs*</div>
+					<div className={`services-header text-center pt-8 pb-8 bg-slate-50 text-black text-2xl ${ bebasNeue.className }`}>Tarifs</div>
 					<div className='sm:grid grid-cols-3'>
 						<div className='hidden sm:flex' />
 						<div className='services-content flex flex-col items-center gap-5'>
 							<h3 className='underline underline-offset-4'>Particuliers</h3>
 							<div className='flex flex-col'>
 								<span className='font-bold'>Dessin simple</span>
-								<span>De 100 à 200 euros</span>
+								<span>de 100 à 200 euros</span>
 							</div>
 							<div className='flex flex-col'>
 								<span className='font-bold'>Dessin création</span>
-								<span>De 150 à 300 euros</span>
+								<span>de 150 à 300 euros</span>
 							</div>
 						</div>
 						<div className='hidden sm:flex'/>
@@ -40,47 +40,47 @@ function ServicesPage(){
 							<div className='sm:ml-auto flex flex-col gap-5 pl-4 pr-4'>
 								<div className='flex sm:flex-col justify-between'>
 									<span className='font-bold'>Dessin simple</span>
-									<span>De 200 à 400 euros</span>
+									<span>de 200 à 400 euros</span>
 								</div>
 								<div className='flex sm:flex-col justify-between'>
 									<span className='font-bold'>Dessin création</span>
-									<span>De 300 à 600 euros</span>
+									<span>de 300 à 600 euros</span>
 								</div>
 							</div>
 							<div className='pl-4 pr-4 ml-auto mr-auto sm:m-0'>
 								<span className='font-bold'>Droits d'utilisation</span>
 								<div className='flex flex-col'>
-									<span>Site web: 1000 euros (1 an)</span>
-									<span>Catalogue: 300 euros (1 mois)</span>
+									<span>Site web: 1 000 euros/année</span>
+									<span>Catalogue: 300 euros/année</span>
 								</div>
 							</div>
 						</div>
 					</div>
 					<Divider className='h-2 bg-white min-[815px]:h-[0.5px]'/>
 					<div className='services-content flex flex-col gap-5'>
-						<h3 className='underline underline-offset-4'>Magazines / Sites WEB</h3>
+						<h3 className='underline underline-offset-4'>Magazines / Sites web</h3>
 						<div className='grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-32'>
 							<div className='sm:ml-auto flex flex-col gap-5 pl-4 pr-4'>
 								<div className='flex sm:flex-col justify-between'>
 									<span className='font-bold'>Dessin simple</span>
-									<span>De 300 à 400 euros</span>
+									<span>de 300 à 400 euros</span>
 								</div>
 								<div className='flex sm:flex-col justify-between'>
 									<span className='font-bold'>Dessin création</span>
-									<span>De 400 à 900 euros</span>
+									<span>de 400 à 900 euros</span>
 								</div>
 							</div>
 							<div className='pl-4 pr-4 ml-auto mr-auto sm:m-0'>
 								<span className='font-bold'>Droits d'utilisation</span>
 								<div className='flex flex-col'>
-									<span>Site web: 1000 euros (1 an)</span>
-									<span>Catalogue: 300 euros (1 mois)</span>
+									<span>Site web: 1 000 euros/année</span>
+									<span>Catalogue: 300 euros/année</span>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className='services-footer flex flex-col text-center pt-8 pb-8 bg-slate-50 text-black'>
-						<span>* Les tarifs sont à titre indicatif.</span>
+						<span>Les tarifs sont à titre indicatif.</span>
 						<span>Une devis sera réalisé à chaque demande spécifique du client.</span>
 					</div>
 				</section>
