d9e81bf8af990c0f88f1c5baec1e22ecb445438d
dkm94 <diane.mpacko@gmail.com>
2024-09-19 18:01:34 +0200

    - center intro text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 
-import { Divider } from '@mui/material';
+import { Divider, IconButton } from '@mui/material';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import { PageContent, PageHeader } from '@/components';
 import Image from 'next/image';
 
@@ -20,10 +21,10 @@ const AboutPage = () => {
 					<div className='flex flex-col px-16 min-[1200px]:flex-row max-w-[1175px] mt-14 md:mt-24 gap-4'>
 						<div className='relative flex items-center'>
 							<div className='absolute bg-white opacity-50 w-full h-full' aria-hidden="true"/>
-							<div className='relative px-10 pt-16 pb-8 flex flex-col gap-6 text-sm'>
+							<div className='relative px-10 pt-16 pb-8 flex flex-col gap-6 text-sm text-center'>
 								<p className='text-pretty'>{'C\'est avec un grand plaisir que je vous accueille sur ma galerie.'}</p>
 								<p>{'Je m\'appelle Virginie, j\'ai grandi dans la région parisienne. Originaire d\'une famille modeste, je n\'ai jamais été en école d\'art ni pris de cours. Je suis une autodidacte, passionnée de création et de mode depuis toujours. J\'ai appris à force de persévérance, mais aussi d\'expériences grâce à des entreprises qui m\'ont fait confiance.'}</p>
-								<p>{'Merci pour votre visite. Prenez soin de vous ✨'}</p>
+								<p>{'Merci pour votre visite. Prenez soin de vous '}<AutoAwesomeIcon />	 </p>
 								<div className='flex w-full justify-end'>
 									<Image src='/assets/signature.png' alt='signature' width={200} height={100} className='h-14 md:h-22'/>
 								</div>
