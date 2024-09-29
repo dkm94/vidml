504a06705f76cf86bf84fb7e89142633c89195e2
dkm94 <diane.mpacko@gmail.com>
2024-09-30 01:44:36 +0200

    + add emailjs

--- components/contact-form/ContactForm.tsx
+++ components/contact-form/ContactForm.tsx
@@ -2,23 +2,44 @@
 'use client';
 import { inter } from '@/app/fonts';
 
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent, useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import { ContactFormInput, ContactFormTextArea, SubmitButton } from '..';
 
 const ContactForm = () => {
+	const form: any = useRef();
 	const [ name, setName ] = useState<string>('');
 	const [ email, setEmail ] = useState<string>('');
 	const [ subject, setSubject ] = useState<string>('');
 	const [ content, setContent ] = useState<string>('');
 
+	const [ message, setMessage ] = useState<string>('');
+
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log(name, email, subject, content);
+		
+		emailjs
+			.sendForm(`${ process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID }`, `${ process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID }` , form.current, { publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY, })
+		  .then(
+				() => {
+					setMessage('Votre mail a été envoyé.');
+					setTimeout(() => {
+						setMessage('');
+					}, 5000);
+				},
+				(error) => {
+					setMessage('Oups, l\'envoi de votre mail a échoué. Contactez-moi par mail vidml@gmail.com');
+					setTimeout(() => {
+						setMessage('');
+					}, 5000);
+				},
+		  );
 	};
 
 	return (
 		<div className='flex justify-center mt-20 w-full'>
-			<form onSubmit={handleSubmit} className={`${ inter.className } flex flex-col w-full max-w-2xl bg-[#8B8B8B] py-12 px-10 gap-6 border border-solid border-gray-50`}>
+			<form ref={form} onSubmit={handleSubmit} className={`${ inter.className } flex flex-col w-full max-w-2xl bg-[#8B8B8B] py-12 px-10 gap-6 border border-solid border-gray-50`}>
 				<div className='flex flex-col gap-6 text-xs'>
 					<ContactFormInput label='Nom' type='text' name="name" onChange={(e) => setName(e.target.value)} />
 					<ContactFormInput label='Email' type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
@@ -28,6 +49,7 @@ const ContactForm = () => {
 				<div className='flex justify-end'>
 					<SubmitButton className="border-black border-solid border-[1px] text-xs" text={'Envoyer'} type={'submit'} />
 				</div>
+				<span className='text-slate-200 text-center text-xs'>{message}</span>
 			</form>
 		</div>
 	);
