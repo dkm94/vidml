38984565effb1b0cd6cbbb229ff179bd9820feee
dkm94 <diane.mpacko@gmail.com>
2024-09-29 20:33:07 +0200

    + fixed tap highlight

--- components/Navlink.tsx
+++ components/Navlink.tsx
@@ -10,7 +10,7 @@ const Navlink = ({ id, title, path, setShowLinks }: NavLinkProps) => {
 	const currentPath = usePathname();
 
 	return (
-		<li key={id} className={'navbar__item block lg:translate-y-0 slideDown-1'}>
+		<li key={id} className={'navbar__item navlink block lg:translate-y-0 slideDown-1'}>
 			<Link
 				className={`${ inter.className } ${ currentPath === path ? 'font-black active-link' : 'font-light' } px-[5px] text-gray-50 lg:text-slate-900 no-underline block p-4 bg-inherit uppercase text-sm focus:outline-none active:bg-transparent`}
 				href={path}
